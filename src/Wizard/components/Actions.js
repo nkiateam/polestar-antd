eb9d83ee6ed654fa56e9ed0d9b205564e2f387eb
Hyun Seung Jeong <ahribori@gmail.com>
2017-11-19 10:04:38 +0900

    - Wizard 컴포넌트 Action 영역 리팩토링

--- src/Wizard/components/Actions.js
+++ src/Wizard/components/Actions.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import Button from '../../Button';
 import shortId from 'shortid';
+import Button from '../../Button';
 
 class Actions extends React.Component {
+    renderNextButton = () => (this.props.current < this.props.stepLength - 1
+        ? <Button type="primary" onClick={this.props.handleNext}>{this.props.nextButtonLabel}</Button> : null);
+
+    renderPreviousButton = () => (this.props.current > 0
+        ? <Button onClick={this.props.handlePrev}>
+            {this.props.prevButtonLabel}</Button> : null);
+
+    renderDoneButton = () => (this.props.current === this.props.stepLength - 1
+        ? <Button type="primary" onClick={this.props.handleDone}>{this.props.doneButtonLabel}</Button> : null);
+
+    renderCustomButtons = () => (
+        this.props.customButtons.map(node => React.cloneElement(node, {
+            key: shortId.generate(),
+        }))
+    );
+
     render() {
         return (
             <div className="steps-action polestar-wizard-actions">
-                { // Next Button
-                    this.props.current < this.props.stepLength - 1
-                    &&
-                    <Button type="primary" onClick={this.props.handleNext}>{this.props.nextButtonLabel}</Button>
-                }
-                { // Done Button
-                    this.props.current === this.props.stepLength - 1
-                    &&
-                    <Button type="primary" onClick={this.props.handleDone}>{this.props.doneButtonLabel}</Button>
-                }
-                { // Previous Button
-                    this.props.current > 0
-                    &&
-                    <Button style={{ marginLeft: 8 }} onClick={this.props.handlePrev}>
-                        {this.props.prevButtonLabel}</Button>
-                }
-                {this.props.customButtons.map(node => React.cloneElement(node, {
-                    key: shortId.generate(),
-                }))}
+                <Button.Spacing>
+                    { this.renderNextButton() }
+                    { this.renderDoneButton() }
+                    { this.renderPreviousButton() }
+                    { this.renderCustomButtons() }
+                </Button.Spacing>
             </div>
         );
     }
